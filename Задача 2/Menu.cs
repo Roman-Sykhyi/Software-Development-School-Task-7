b7aa3b72b19844c2a5b9ce5cb9312315e01cf911
Roman-Sykhyi <kariys2015@gmail.com>
2021-10-14 15:54:42 +0300

    Виконано задачу 2 про меню

---
+++ Задача 2/Menu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Задача_2
+{
+    public class Menu
+    {
+        Dictionary<string, float> products;
+        Dictionary<string, int> prices;
+
+        public Menu(string menuFilePath, string pricesFilePath)
+        {
+            products = new Dictionary<string, float>();
+            prices = new Dictionary<string, int>();
+
+            FillPricesFromFile(pricesFilePath);
+            FillProductsFromFile(menuFilePath);
+        }
+
+        public string GetProductsInfo()
+        {
+            string result = "";
+
+            foreach(var item in products)
+            {
+                result += $"{item.Key,-10}  {item.Value,-10} ";
+                result += (prices[item.Key] * item.Value).ToString("0.0") + "\n";
+            }
+
+            return result;
+        }
+
+        private void FillPricesFromFile(string filePath)
+        {
+            StreamReader file = new StreamReader(filePath);
+            string line;
+
+            while((line = file.ReadLine()) != null)
+            {
+                string[] productPrice = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                prices.Add(productPrice[0], int.Parse(productPrice[1]));
+            }
+        }
+
+        private void FillProductsFromFile(string productsFilePath)
+        {
+            StreamReader file = new StreamReader(productsFilePath);
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] product = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (product.Length != 2)
+                    continue;
+
+                string productName = product[0];
+                float productWeight = float.Parse(product[1], CultureInfo.InvariantCulture);
+
+                if(!products.ContainsKey(productName))
+                {
+                    products.Add(productName, productWeight);
+                }
+                else
+                {
+                    products[productName] += productWeight;
+                }
+            }
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pricesFilePath = @"E:\Sigma Pract\Завдання 7\Задача 2\prices.txt";
+            string menuFilePath = @"E:\Sigma Pract\Завдання 7\Задача 2\menu.txt";
+
+            Menu menu = new Menu(menuFilePath, pricesFilePath);
+            Console.WriteLine(menu.GetProductsInfo());
+        }
+    }
+}
