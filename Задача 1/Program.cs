12022ed333a25b0bb05ca8d23aa9b4e6b696eabc
Roman-Sykhyi <kariys2015@gmail.com>
2021-10-14 14:57:59 +0300

    Виконано задачу 1 про переклад тексту

---
+++ Задача 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>()
+            {
+                { "I", "Boy" },
+                { "go", "run" },
+                { "to", "to"},
+                { "school", "cinema"}
+            };
+
+            TranslatedText translatedText = new TranslatedText("I go to school. Girl runs to school", dictionary);
+            Console.WriteLine(translatedText.value);
+        }
+    }
+}
---
+++ Задача 1/TranslatedText.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задача_1
+{
+    public class TranslatedText
+    {
+        public string value { get; private set; }
+
+        private Dictionary<string, string> dictionary;
+        private char[] delimiters = { ' ', ',', '.', ':', '\t' };
+
+        public TranslatedText(string text, Dictionary<string, string> dictionary)
+        {
+            value = text;
+            this.dictionary = dictionary;
+
+            string[] words = value.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            FillDictionary(words);
+            TranslateText(words);
+        }
+
+        private void TranslateText(string[] words)
+        {
+            foreach(string word in words)
+            {
+                value = value.Replace(word, dictionary.GetValueOrDefault(word));
+            }
+        }
+
+        private void FillDictionary(string[] words)
+        {
+            foreach (string word in words)
+            {
+                if (!dictionary.ContainsKey(word))
+                {
+                    string translation = GetTranslation(word);
+                    AddWordToDictionary(word, translation);
+                }
+            }
+        }
+
+        private void AddWordToDictionary(string word, string translation)
+        {
+            dictionary.Add(word, translation);
+        }
+
+        private string GetTranslation(string word)
+        {
+            Console.Write("Enter translation for " + word + ": ");
+            return Console.ReadLine();
+        }
+    }
+}
