f928661daad211727d887479f364f8bfc027af1c
Roman-Sykhyi <kariys2015@gmail.com>
2021-10-19 13:43:45 +0300

    Виправлено помилки в завданні 2. Розділено класи, використано using і опрацьовано винятки при роботі з файлом.

---
+++ Задача 2/IngredentsInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Задача_2
+{
+    public class IngredentsInfo
+    {
+        public Dictionary<string, float> Values { get; }
+
+        public IngredentsInfo(string filePath)
+        {
+            Values = new Dictionary<string, float>();
+            FillWeightsFromFile(filePath);
+        }
+        private void FillWeightsFromFile(string filePath)
+        {
+            #region Перевірки
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentException("Шлях до файлу не може бути пустим.", nameof(filePath));
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("Файлу за вказаним шляхом не знайдено.", nameof(filePath));
+            }
+
+            if (string.Compare(new FileInfo(filePath).Extension, ".txt") != 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(filePath), "Доступно тільки розширення файлу .txt .");
+            }
+            #endregion
+
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] product = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    if (product.Length != 2)
+                        continue;
+
+                    string productName = product[0];
+
+                    if (!float.TryParse(product[1], NumberStyles.Float, CultureInfo.InvariantCulture, out float productWeight))
+                    {
+                        throw new FormatException("Помилка читання ваги продукту з файлу. Неправильно заповнено файл.");
+                    }
+
+                    if (!Values.ContainsKey(productName))
+                    {
+                        Values.Add(productName, productWeight);
+                    }
+                    else
+                    {
+                        Values[productName] += productWeight;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Задача 2/IngredientsPrices.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Задача_2
+{
+    public class IngredientsPrices
+    {
+        public Dictionary<string, int> Values { get; }
+
+        public IngredientsPrices(string filePath)
+        {
+            Values = new Dictionary<string, int>();
+            FillPricesFromFile(filePath);
+        }
+
+        private void FillPricesFromFile(string filePath)
+        {
+            #region Перевірки
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentException("Шлях до файлу не може бути пустим.", nameof(filePath));
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("Файлу за вказаним шляхом не знайдено.", nameof(filePath));
+            }
+
+            if (string.Compare(new FileInfo(filePath).Extension, ".txt") != 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(filePath), "Доступно тільки розширення файлу .txt .");
+            }
+            #endregion
+
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] productPrice = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    if (!int.TryParse(productPrice[1], out int price))
+                    {
+                        throw new FormatException("Помилка читання ціни продукту з файлу. Неправильно заповнено файл.");
+                    }
+
+                    Values.Add(productPrice[0], price);
+                }
+            }
+        }
+    }
+}
